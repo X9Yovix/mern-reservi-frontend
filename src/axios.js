cc006ce6d9783636840a2830e231ed21862619b5
Karim Ouazzou <karimouazzou@gmail.com>
2024-02-29 18:50:07 +0100

    update token structure

--- src/axios.js
+++ src/axios.js
@@ -14,7 +14,7 @@ instance.interceptors.request.use(
   (config) => {
     const token = getToken()
     if (token) {
-      config.headers.Authorization = token
+      config.headers.Authorization = `Bearer ${token}`
     }
     return config
   },
