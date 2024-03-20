1cb9e379d55ca780441fd0c0299aee6e3a9cbfee
Karim Ouazzou <karimouazzou@gmail.com>
2024-03-20 20:09:55 +0100

    reservations: update reservation by the user

--- src/Components/User/Reservations/Reservations.js
+++ src/Components/User/Reservations/Reservations.js
@@ -1,8 +1,12 @@
-import { useEffect, useState } from "react"
-import { Button, Modal, Pagination, Space, Spin, Table, Tag, message } from "antd"
+import { useCallback, useEffect, useState } from "react"
+import { Spin, Tag, Form, Button, DatePicker, Input, InputNumber, message, Modal, Pagination, Table, Space } from "antd"
+import { CloseCircleOutlined, EditOutlined } from "@ant-design/icons"
 import axios from "../../../axios"
+import dayjs from "dayjs"
 import "./Reservations.css"
-import { CloseCircleOutlined } from "@ant-design/icons"
+
+const { TextArea } = Input
+const { RangePicker } = DatePicker
 
 const Reservations = () => {
   const [loading, setLoading] = useState(false)
@@ -10,6 +14,131 @@ const Reservations = () => {
   const [additionalInfo, setAdditionalInfo] = useState(null)
   const [totalPages, setTotalPages] = useState(0)
   const [currentPage, setCurrentPage] = useState(1)
+  const [editModalVisible, setEditModalVisible] = useState(false)
+  const [editReservation, setEditReservation] = useState(null)
+  const [reservedDates, setReservedDates] = useState([])
+
+  const [editReservationForm] = Form.useForm()
+
+  const fetchReservedDates = async (roomId) => {
+    setLoading(true)
+    try {
+      await axios
+        .get(`/reservations/${roomId}`)
+        .then((res) => {
+          setReservedDates(res.data.reservations)
+          setLoading(false)
+        })
+        .catch((err) => {
+          console.log(err)
+          setLoading(false)
+        })
+    } catch (error) {
+      console.error("Error occurred while fetching reserved dates:", error)
+    }
+  }
+
+  const cellRender = useCallback(
+    (date, info) => {
+      if (info.type !== "date") {
+        return info.originNode
+      }
+      if (typeof date === "number") {
+        return <div className="ant-picker-cell-inner">{date}</div>
+      }
+
+      const reservedPendingDates = reservedDates.filter((reservation) => reservation.status === "pending" && reservation._id !== editReservation.key)
+      const isPending = reservedPendingDates.some((reservation) => {
+        const startDate = new Date(reservation.start_date).setHours(0, 0, 0, 0)
+        const endDate = new Date(reservation.end_date).setHours(0, 0, 0, 0)
+        return date && new Date(date).getTime() >= startDate && new Date(date).getTime() <= endDate
+      })
+
+      const reservedAcceptedDates = reservedDates.filter((reservation) => reservation.status === "confirmed")
+      const isReserved = reservedAcceptedDates.some((reservation) => {
+        const startDate = new Date(reservation.start_date).setHours(0, 0, 0, 0)
+        const endDate = new Date(reservation.end_date).setHours(0, 0, 0, 0)
+        return date && new Date(date).getTime() >= startDate && new Date(date).getTime() <= endDate
+      })
+
+      const styleIsPending = { backgroundColor: "darkred", color: "white" }
+      const styleIsReserved = { backgroundColor: "green", color: "white" }
+
+      const cellStyle = isPending ? styleIsPending : isReserved ? styleIsReserved : {}
+
+      return (
+        <div className="ant-picker-cell-inner" style={cellStyle}>
+          {date.date()}
+        </div>
+      )
+    },
+    [reservedDates]
+  )
+
+  const handleUpdate = (item) => {
+    const formattedStartDate = dayjs(item.start_date)
+    const formattedEndDate = dayjs(item.end_date)
+    const updatedItem = {
+      ...item,
+      start_date: formattedStartDate,
+      end_date: formattedEndDate
+    }
+    fetchReservedDates(item.room[0])
+    editReservationForm.setFieldsValue({
+      participants: updatedItem.participants,
+      reservation_range: [updatedItem.start_date, updatedItem.end_date],
+      additional_info: updatedItem.additional_info
+    })
+    console.log(updatedItem)
+    setEditReservation(updatedItem)
+    setEditModalVisible(true)
+  }
+
+  const handleEditModalCancel = () => {
+    editReservationForm.resetFields(["participants", "reservation_range", "additional_info"])
+    setEditModalVisible(false)
+    setEditReservation(null)
+  }
+
+  const handleEditModalOk = () => {
+    editReservationForm.submit()
+  }
+
+  const handleUpdateRowReservation = async (values) => {
+    try {
+      setLoading(true)
+      const reservationId = editReservation.key
+      const payload = {
+        participants: values.participants,
+        reservation_range: values.reservation_range,
+        additional_info: values.additional_info,
+        meeting_rooms: editReservation.room[0]
+      }
+      await axios.put(`reservations/user/update/${reservationId}?page=${currentPage}`, payload).then((res) => {
+        const data = []
+        res.data.reservations.map((reservation) => {
+          data.push({
+            key: reservation._id,
+            room: [reservation.meeting_rooms._id, reservation.meeting_rooms.name],
+            categories: reservation.meeting_rooms.categories,
+            capacity: reservation.meeting_rooms.capacity,
+            start_date: new Date(reservation.start_date).toLocaleDateString("en-GB", { day: "numeric", month: "long", year: "numeric" }),
+            end_date: new Date(reservation.end_date).toLocaleDateString("en-GB", { day: "numeric", month: "long", year: "numeric" }),
+            participants: reservation.participants,
+            additional_info: reservation.additional_info,
+            status: reservation.status
+          })
+        })
+        setReservations(data)
+        setTotalPages(res.data.totalPages)
+        setLoading(false)
+        message.success(res.data.message)
+      })
+    } catch (error) {
+      console.error(error)
+      setLoading(false)
+    }
+  }
 
   const handleAdditionalInfo = (additional_info) => {
     setAdditionalInfo(additional_info)
@@ -50,7 +179,7 @@ const Reservations = () => {
           ) : status == "confirmed" ? (
             <Tag color="green">Approved</Tag>
           ) : status == "rejected" ? (
-            <Tag color="green">Rejected</Tag>
+            <Tag color="red">Rejected</Tag>
           ) : (
             <Tag color="orange">Canceled</Tag>
           )}
@@ -76,20 +205,27 @@ const Reservations = () => {
       title: "Additional Information",
       dataIndex: "additional_info",
       key: "additional_info",
-      render: (_, { additional_info }) => (
-        <Button type="text" onClick={() => handleAdditionalInfo(additional_info)}>
-          View Info
-        </Button>
-      )
+      render: (_, { additional_info }) =>
+        additional_info && additional_info.length > 0 ? (
+          <Button type="text" onClick={() => handleAdditionalInfo(additional_info)}>
+            View Info
+          </Button>
+        ) : (
+          "No additional information"
+        )
     },
+
     {
       title: "Action",
       key: "action",
-      render: (_, { key, status }) =>
-        status === "pending" && (
+      render: (_, item) =>
+        item.status === "pending" && (
           <Space size="middle">
-            <Button className="decline-btn" icon={<CloseCircleOutlined />} onClick={() => handleCancelRequest(key)}>
+            <Button className="decline-btn" icon={<CloseCircleOutlined />} onClick={() => handleCancelRequest(item.key)}>
               Cancel
+            </Button>
+            <Button icon={<EditOutlined />} onClick={() => handleUpdate(item)}>
+              Update
             </Button>
           </Space>
         )
@@ -106,6 +242,7 @@ const Reservations = () => {
             key: reservation._id,
             room: [reservation.meeting_rooms._id, reservation.meeting_rooms.name],
             categories: reservation.meeting_rooms.categories,
+            capacity: reservation.meeting_rooms.capacity,
             start_date: new Date(reservation.start_date).toLocaleDateString("en-GB", { day: "numeric", month: "long", year: "numeric" }),
             end_date: new Date(reservation.end_date).toLocaleDateString("en-GB", { day: "numeric", month: "long", year: "numeric" }),
             participants: reservation.participants,
@@ -137,6 +274,7 @@ const Reservations = () => {
               key: reservation._id,
               room: [reservation.meeting_rooms._id, reservation.meeting_rooms.name],
               categories: reservation.meeting_rooms.categories,
+              capacity: reservation.meeting_rooms.capacity,
               start_date: new Date(reservation.start_date).toLocaleDateString("en-GB", { day: "numeric", month: "long", year: "numeric" }),
               end_date: new Date(reservation.end_date).toLocaleDateString("en-GB", { day: "numeric", month: "long", year: "numeric" }),
               participants: reservation.participants,
@@ -176,6 +314,96 @@ const Reservations = () => {
       )}
       <Modal title="Additional Information" open={!!additionalInfo} onCancel={handleCloseModal} footer={null}>
         <p>{additionalInfo}</p>
+      </Modal>
+
+      <Modal title="Edit Reservation" open={editModalVisible} onCancel={handleEditModalCancel} onOk={handleEditModalOk} confirmLoading={loading}>
+        {editReservation && (
+          <Form form={editReservationForm} name="edit-reservation-form" onFinish={handleUpdateRowReservation}>
+            <Form.Item
+              name="participants"
+              label="Participants"
+              rules={[
+                { required: true, message: "Please input number of participants" },
+                { type: "number", min: 1, message: "Number of participants must be at least 1" },
+                () => ({
+                  validator(_, value) {
+                    if (value && value > editReservation.capacity) {
+                      return Promise.reject(`Number of participants cannot exceed ${editReservation.capacity}`)
+                    }
+                    return Promise.resolve()
+                  }
+                })
+              ]}
+            >
+              <InputNumber min={1} />
+            </Form.Item>
+            <Form.Item
+              name="reservation_range"
+              label="Reservation Date & Time"
+              rules={[
+                { required: true, message: "Please select reservation date and time" },
+                ({ getFieldValue }) => ({
+                  validator() {
+                    const selectedDates = getFieldValue("reservation_range")
+                    if (!selectedDates || !selectedDates[0] || !selectedDates[1]) {
+                      return Promise.resolve()
+                    }
+                    const selectedRange = [selectedDates[0].toDate(), selectedDates[1].toDate()]
+
+                    for (const reservation of reservedDates) {
+                      const startDate = new Date(reservation.start_date)
+                      const endDate = new Date(reservation.end_date)
+                      if (
+                        (selectedRange[0] >= startDate && selectedRange[0] <= endDate) ||
+                        (selectedRange[1] >= startDate && selectedRange[1] <= endDate) ||
+                        (selectedRange[0] <= startDate && selectedRange[1] >= endDate)
+                      ) {
+                        // check if its in old range dates
+                        if (
+                          (editReservation.start_date >= startDate && editReservation.start_date <= endDate) ||
+                          (editReservation.end_date >= startDate && editReservation.end_date <= endDate) ||
+                          (editReservation.start_date <= startDate && editReservation.end_date >= endDate)
+                        ) {
+                          continue
+                        }
+                        // check if it overlaps with other reservations
+                        if (reservation.status === "pending" || reservation.status === "confirmed") {
+                          return Promise.reject("Selected date range is overlapping with an existing reservation")
+                        }
+                      }
+                    }
+                    return Promise.resolve()
+                  }
+                })
+              ]}
+            >
+              <RangePicker
+                cellRender={cellRender}
+                disabledDate={(current) => {
+                  const reservationsFiltered = reservedDates.filter(
+                    (reservation) => reservation.status !== "rejected" && reservation.status !== "canceled" && reservation._id !== editReservation.key
+                  )
+                  for (const reservation of reservationsFiltered) {
+                    const startDate = new Date(reservation.start_date).setHours(0, 0, 0, 0)
+                    const endDate = new Date(reservation.end_date).setHours(0, 0, 0, 0)
+                    if (current && new Date(current.$d) >= startDate && new Date(current.$d) <= endDate) {
+                      return true
+                    }
+                  }
+                  //const today = new Date().setHours(0, 0, 0, 0)
+                  const today = new Date()
+                  if (current < today) {
+                    return true
+                  }
+                  return false
+                }}
+              />
+            </Form.Item>
+            <Form.Item name="additional_info" label="Additional Information">
+              <TextArea rows={4} />
+            </Form.Item>
+          </Form>
+        )}
       </Modal>
     </Spin>
   )
